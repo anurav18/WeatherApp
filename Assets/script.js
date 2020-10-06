4163179c1e48682c1f2e6a6dc32e8db1fa25c82f
Anu Rav <anusharavva@gmail.com>
2020-10-05 22:45:08 -0700

    Basic layout and functionality working

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,22 @@
 
 let citiesBlockEl = $(".selected-cities");
 $("#add-city").on("click",function(event){
-    event.preventDefault();var cityInput = $("#city-input").val();
-    let newDivEl = $("<div>");
-    newDivEl.attr("class","row");
-    newDivEl.addClass("city-block");
-    let newSpanEl = $("<span>");
-    newSpanEl.attr("class","city-span");
-    newDivEl.attr("cityName",cityInput);
-    console.log(cityInput);
-    newSpanEl.text(cityInput);
-    citiesBlockEl.append(newDivEl);
-    newDivEl.append(newSpanEl);
+    event.preventDefault();
+    var cityInput = $("#city-input").val();
+    let ulEl = $("#cities-list");
+    let newliEl = $("<li class='list-group-item'>");
+    newliEl.text(cityInput);
+    ulEl.append(newliEl);
+    newliEl.attr("cityName",cityInput);
+   $("#add-city").attr("cityName",cityInput);
+   displayWeatherInfo(cityInput);
 
 });
 
 
 var apikey = "5a16da0f6c2273bd4b86510d10778287";
-for(var i = 1; i<6;i++)
-{
-    console.log(moment().add(i, 'days').format('L'));
-}
-function displayWeatherInfo(){
-    var city = $(this).attr("cityName");
-    
+
+function displayWeatherInfo(city){
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + apikey;
     $.ajax({
     method:"GET",
@@ -31,10 +24,10 @@ function displayWeatherInfo(){
     }).then(function(response){
 console.log(response);
 $(".display-Name").text(response.name +"("+ moment().format('L')+")");
-$(".display-Temparature").text(response.main.temp);
-$(".display-Humidity").text(response.main.humidity);
-$(".display-Windspeed").text(response.wind.speed);
-console.log(response.weather[0].icon);
+
+$(".display-Temparature").html("Temparature: "+ (convertKtoF(response.main.temp)).toFixed(2) + "&deg;F");
+$(".display-Humidity").text("Humidity: "+response.main.humidity+"%");
+$(".display-Windspeed").text("Wind Speed: "+response.wind.speed+"MPH");
 var imageSrc = " http://openweathermap.org/img/wn/"+response.weather[0].icon+".png";
 $("#cloud-Image").attr("src",imageSrc);
 var long = response.coord.lon;
@@ -43,21 +36,27 @@ CalculateUVIndex(lat, long);
 forecast(city);
     })
 
-
 }
+
+//Temparature conversion 
+
+function convertKtoF(tempInKelvin) {
+    // (360K − 273.15) × 9/5 + 32 = 188.33°F
+    return ((tempInKelvin - 273.15) * 9) / 5 + 32;
+  }
 
 //calculate UV Index
 
 function CalculateUVIndex(lat,long){
-    // var apikey = "5a16da0f6c2273bd4b86510d10778287";
    var queryURL =  "http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+long+"&appid="+apikey;
-// 
+
 $.ajax({
     url: queryURL,
     method:"GET"
 }).then(function(response){
     console.log(response);
-    $(".display-UVindex").text(response.value);
+    // $(".display-UVindex").text("UV Index: "+ response.value);
+    $(".display-UVindex").html("UV Index: <span style='background-color:red'>"+response.value+"</span>");
     console.log("UV index calculated");
 })
 }
@@ -74,10 +73,9 @@ function forecast(city)
         method:"GET"
 
     }).then(function(response){
-
-    
     for(var i = 0 ; i < 5; i++)
     {
+        $(".Heading-2").text("5-Day Forecast:");
         var maindivEl = $("<div class='card text-white bg-primary mb-3'>");
         newRowEl.append(maindivEl);
         var cardbodyEl = $("<div class='card-body'>");
@@ -94,31 +92,17 @@ function forecast(city)
         cardbodyEl.append(cardtempEl);
         var cardhumidityEl = $("<p class='card-text'>");
         cardbodyEl.append(cardhumidityEl);
-
-        //text for card title
         cardtitleEl.text(moment().add(i+1, 'days').format('L'));
-
-        //text for humidity
-
-        cardhumidityEl.text(response.list[i].main.humidity);
-
-        //text for temparature
-
-        cardtempEl.text(response.list[i].main.temp);
-
-        //setting image url
-
-        
-
-
-
-
+        cardhumidityEl.text("Humidity: "+response.list[i].main.humidity+"%");
+        cardtempEl.html("Temp: "+(convertKtoF(response.list[i].main.temp)).toFixed(2) + "&deg;F");
     }
-
-
-    });
+  });
 }
 
+//On Click event handler for all the elements declared with class city-block
 
-
-$(document).on("click",".city-block",displayWeatherInfo);
+$(document).on("click","li",function(event){
+event.preventDefault();
+var city = $(this).attr("cityName");
+displayWeatherInfo(city);
+});
